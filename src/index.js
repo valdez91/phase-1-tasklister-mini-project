eea1363b88202aaebfe0f873a32165ae56a6ba5f
Victor Kemboi <vickemtoo97@gmail.com>
2024-04-03 21:20:21 +0300

    tasklistener -added an EventListener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementsByTagName("form")[0].addEventListener("submit", (event) => {
+    event.preventDefault();
+  })
 });
